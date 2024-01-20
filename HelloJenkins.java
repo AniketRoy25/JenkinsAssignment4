bea014922a39a6ae64841f15b6b90689151b558c
Aniket <aniketroy97@outlook.com>
2024-01-20 09:16:55 +0000

    added three files for assignment 4

---
+++ HelloJenkins.java
@@ -0,0 +1,26 @@
+class HelloJenkins{
+	public static void main(String[] args){
+	
+		System.out.println("Hello Jenkins!");
+	}
+
+}
+
+:wq
+:wq
+:wq
+:wq
+:wq
+:wq
+:wq
+:wq
+:wq
+:wq
+:wq
+:wq
+:wq
+:wq
+:wq
+:wq
+:wq
+
---
+++ HelloWipro.java
@@ -0,0 +1,13 @@
+class HelloWipro{
+	public static void main(String[] a){
+	
+		System.out.println("Hello Wipro!");
+	}
+
+}
+
+:wq
+
+
+
+
